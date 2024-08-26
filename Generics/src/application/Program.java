1f9436ac38f018d85aeb887f1cb7d5e01067c436
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2024-08-26 15:32:05 -0300

    Creating a generic method

--- Generics/src/application/Program.java
+++ Generics/src/application/Program.java
@@ -11,27 +11,28 @@ public class Program {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		PrintService<Integer> ps = new PrintService<>();
-		
+		/* Accepts values of integer, string, float type. It is type safe. */
+
 		System.out.print("How many values: ");
 		int n = sc.nextInt();
-		
+
 		// ps.addValue("João"); This code will no longer work
-		
-		for(int i = 0; i < n; i++) {
-			Integer value = sc.nextInt(); 
+
+		for (int i = 0; i < n; i++) {
+			Integer value = sc.nextInt();
 			ps.addValue(value);
 		}
-		
+
 		ps.print();
-		
-		Integer x = /*(Integer)*/ps.first();	// No casting is necessary
-		
+
+		Integer x = /* (Integer) */ps.first(); // No casting is necessary
+
 		System.out.println("First: " + x);
-		
+
 		sc.close();
-		
+
 	}
 
 }
---
+++ genericrs.delimited_generic/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module genericrs.delimited_generic {
+}
