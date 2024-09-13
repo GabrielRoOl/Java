42fda679d1b30e523b863b465b16e7ca269c5181
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2024-09-13 18:24:50 -0300

    Sets, HashSet, LinkedHashSet, TreeSet 

--- set/src/application/EqualityTest.java
+++ set/src/application/EqualityTest.java
@@ -14,8 +14,11 @@ public class EqualityTest {
 		set.add(new Product("Notebook", 1200.0));
 		set.add(new Product("Tablet", 400.0));
 		
-		Product prod = new Product("Notebook", 1200.0);
+		//Product prod = new Product("Notebook", 1200.0);
 		
-		System.out.println(set.contains(prod));  // false
+		//System.out.println(set.contains(prod));  // false
+		for(Product p : set) {
+			System.out.println(p);
+		}
 	}
 }
