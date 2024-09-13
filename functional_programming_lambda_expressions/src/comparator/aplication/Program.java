d8c641af5db1b008b1fbda89102caaaebe6c97e3
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2024-09-13 19:27:15 -0300

    Anonymous class

--- functional_programming_lambda_expressions/src/comparator/aplication/Program.java
+++ functional_programming_lambda_expressions/src/comparator/aplication/Program.java
@@ -12,28 +12,18 @@ public class Program {
 
 		List<Product> list = new ArrayList<>();
 
-		list.add(new Product("Tv", 900.0));
-		list.add(new Product("Notebook", 1200.0));
-		list.add(new Product("Tablet", 400.0));
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
 
-		/*
-		 sintaxe usando lambda (função anonima) - arrow function
-		Comparator<Product> compLambda = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-		 */ 
-		
-		/* sintaxe de classe anonima
 		Comparator<Product> comp = new Comparator<Product>() {
-
 			@Override
-			public int compare(Product o1, Product o2) {
-				return o1.getName().toUpperCase().compareTo(o2.getName().toUpperCase());
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 			}
 		};
 
-		list.sort(new IMyComparator());
-		*/
-		
-		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+		list.sort(comp);
 
 		for (Product p : list) {
 			System.out.println(p);
