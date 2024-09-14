b7dcdd9c2897de791ef700423bbdadf669f321c4
Gabriel Rodrigues <144238400+GabrielRoOl@users.noreply.github.com>
2024-09-14 15:50:01 -0300

    inline lambda expression

--- functional_programming_lambda_expressions/src/functional_inteface/predicate/application/Main.java
+++ functional_programming_lambda_expressions/src/functional_inteface/predicate/application/Main.java
@@ -9,25 +9,22 @@ import comparator.entities.Product;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		List<Product> list = new ArrayList<>();
-		
+
 		list.add(new Product("Tv", 900.00));
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("HD Case", 80.90));
 
-		
 		double min = 100.0;
-		
-		Predicate<Product> pred = p -> p.getPrice() >= min;
-		
-		list.removeIf(pred);
-		
-		for(Product p : list) {
+
+		list.removeIf(p -> p.getPrice() >= min);
+
+		for (Product p : list) {
 			System.out.println(p);
 		}
-		
+
 	}
 
 }
