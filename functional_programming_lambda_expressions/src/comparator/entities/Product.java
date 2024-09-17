e339d4bf4048b8b2824c5f753c9d3aff6052689e
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2024-09-17 14:02:28 -0300

    interface implementation

--- functional_programming_lambda_expressions/src/comparator/entities/Product.java
+++ functional_programming_lambda_expressions/src/comparator/entities/Product.java
@@ -39,7 +39,7 @@ public class Product {
 	
 	@Override
 	public String toString() {
-		return "Product name = " + name + ", price = " + price;
+		return "Product = " + name + ", price: " + String.format("%.2f", price);
 	}
 	
 	
---
+++ functional_programming_lambda_expressions/src/function_interface/consumer/application/Main.java
@@ -0,0 +1,22 @@
+package function_interface.consumer.application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import comparator.entities.Product;
+import function_interface.consumer.util.PriceUpdate;
+
+public class Main {
+	public static void main(String[] args) {
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Tablet", 350.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("HD Case", 80.90));
+		
+		list.forEach(new PriceUpdate());
+		
+		list.forEach(System.out::println);;
+	}
+}
